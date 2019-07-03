7f20138c2c7d45a76924cafd7a0ae2f882cad1af
Matt Cadorette <mcadorette@gmail.com>
2019-07-03 13:04:31 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/jul3springj/Jul3springjApplication.java
@@ -0,0 +1,12 @@
+package com.jul3springj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jul3springjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jul3springjApplication.class, args);
+	}
+}
